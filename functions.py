14d5b7b32fa0cd4c64185cf8b443a107b6dd8579
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-01 15:16:24 -0400

    initial commit

---
+++ functions.py
@@ -0,0 +1,15 @@
+def find_col_names(eng, table_name):
+    inspector = inspect(eng)
+    tables = {}
+    
+    for table in inspector.get_table_names():
+        columns = inspector.get_columns(table)
+        names = []
+        for c in columns:
+            names.append(c['name'])
+            tables[table] = names
+    
+    column_names = tables[table_name]        
+    del column_names[:1] #del first name(index)
+    
+    return column_names
