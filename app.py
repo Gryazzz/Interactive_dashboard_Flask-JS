4a675b4a8824c749735b5c3aa00a555041469c31
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-02 09:58:35 -0400

    in-progress

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, redirect
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+engine = create_engine("sqlite:///Data/belly_button_biodiversity.sqlite")
+Base = automap_base()
 
 dash = Flask(__name__)
 
@@ -7,4 +15,10 @@ def home():
 
     return
 
-def 
+@dash.route('/names')
+def get_names():
+    from functions import find_col_names
+    names = find_col_names(engine, 'samples')
+    del names[:1]
+
+    return names
