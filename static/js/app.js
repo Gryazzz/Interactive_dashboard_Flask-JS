2b6c83bd957412d810545a0501036c8240ea3eff
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-05 08:34:34 -0400

    in-progress

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,18 @@
-var $names = d3.select('#names');
-var names_list='';
+// var $names = d3.select('#names');
 
+
+// add options to select #names
 Plotly.d3.json('/names', function(error, response) {
-    console.log(response);
-    names_list = response;
-})
+    names =  response;
+    names.forEach(name => {
+        var option = d3.select('#names').append('option');
+        option.attr('value', name);
+        option.text(name);
+    });
+});
+
+
+
+
+
 
