475b00969a112f80d378d0add2e107ca2d095675
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-06 07:54:27 -0400

    in-progress

--- static/js/app.js
+++ static/js/app.js
@@ -16,7 +16,7 @@ Plotly.d3.json("/otu", function(error, response) {
     otu = response;
 });
 
-// var sample_otu = new Object();
+// var metadata = new Array();
 
 function optionChanged(sample) {
 
@@ -34,15 +34,14 @@ function optionChanged(sample) {
     });
     Plotly.d3.json(("/metadata/" + sample), function(error, response) {
         metadata = response;
-        // console.log(metadata);
+        render_metadata(metadata);
+        console.log(metadata);
     });
     Plotly.d3.json(("/wfreq/" + sample), function(error, response) {
         wfrequency = response;
     });    
 
 };
-
-// var hover = new Array();
 
 function pieChart(dict ) {
 
@@ -51,7 +50,7 @@ function pieChart(dict ) {
     var labels = Object.values(dict[0])[0].slice(0,10);
     var values = Object.values(dict[0])[1].slice(0,10);
     
-    //extract otu's for hover
+    //extract otu's description for hovertext
     var hover = otu.filter((ot, index) => {
         if (labels.includes(index+1)) {
             return ot
@@ -62,7 +61,7 @@ function pieChart(dict ) {
     var data = [{
         values: values,
         labels: labels,
-        names: hover,
+        hovertext: hover,
         type: 'pie'
     }];
     // console.log(values);
@@ -72,44 +71,38 @@ function pieChart(dict ) {
         height: Plotly.d3.select('.pie').node().offsetHeight,
         width: Plotly.d3.select('.pie').node().offsetWidth,
         autosize: true,
-        hoverinfo: 'label+value+name',
         margin: {
-            t: 20,
-            l: 20,
-            r: 20,
-            b: 20
+            t: 10,
+            l: 10,
+            r: 10,
+            b: 10
         }
     };
 
     Plotly.newPlot(pie_plot, data, layout);
 
     window.addEventListener('resize', function() { Plotly.Plots.resize(pie_plot); });
-    // window.onresize = function() {
-    //     Plotly.Plots.resize(pie_plot);
-    //     var window_width = window.innerWidth;
-    //     var content_div_width = Plotly.d3.select('.pie').node().offsetWidth - 20;
-    //     if (content_div_width > (window_width - 40)) {
-    //         var svg_container = document.getElementById('pie-container').getElementsByClassName('plot-container')[0].getElementsByClassName('svg-container')[0];
-    //         svg_container.style.height = (window_width - 40) + 'px';
-    //         Plotly.Plots.resize(pie_plot);
-    //       }
-    // }   
-    
-};   
-//     // var piePl = document.getElementById('piePlot');
-//     // var piePl = d3.select('#piePlot').node();
-//     var piePlot = pie_pl.node();
+};
 
-//     Plotly.newPlot(piePlot, data);
+function render_metadata(data) {
+    Plotly.d3.select('thead')
+    .append('tr')
+    .append('td')
+    .style('font-weight', 'bold')
+    .html('Sample Metadata');
 
-//     // window.addEventListener('resize', function() { Plotly.Plots.resize(piePl); });
-//     window.onresize = function() {
-//         Plotly.relayout(piePlot, {
-//             'xaxis.autorange': true,
-//             'yaxis.autorange': true
-//         });
-//     };
-// };
+    Plotly.d3.select('tbody').selectAll('tr')
+    .data(Object.keys(data))
+    .enter()
+    .append('tr')
+    .html(function(d) {
+        return `<td>${d}: ${data[d]}</td>`   
+    });
+}
+
+
+
+
 
 
 
