4aa2f88061c975ec0d4fdebabb26b84fac0fb8dc
Gryazzz <33532469+Gryazzz@users.noreply.github.com>
2018-06-08 14:39:33 -0400

    changed import paths

--- bb_app/app.py
+++ bb_app/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/belly_button_biodiversity.sqlite"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
-print(app.config)
+# print(app.config)
 
 db = SQLAlchemy(app)
 
@@ -38,7 +38,8 @@ def home():
 
 @app.route('/names')
 def get_names():
-    from functions import find_col_names
+    # from functions import find_col_names # for local hosting only
+    from bb_app.functions import find_col_names # for flask app
     names = find_col_names(engine, 'samples')
 
     return jsonify(names)
@@ -59,7 +60,8 @@ def get_otu():
 
 @app.route('/metadata/<sample>')
 def metaData_sample(sample):
-    from functions import metasample
+    # from functions import metasample # for local hosting only
+    from bb_app.functions import metasample # for flask app
     metaData = metasample(engine, sample)
 
     return jsonify(metaData)
@@ -68,7 +70,8 @@ def metaData_sample(sample):
 @app.route('/wfreq/<sample>')
 def washing_frequency(sample):
 
-    from functions import find_col_names
+    # from functions import find_col_names # for local hosting only
+    from bb_app.functions import find_col_names
 
     if sample in find_col_names(engine, 'samples'):
         form_sample = int(sample[3:])
@@ -82,7 +85,8 @@ def washing_frequency(sample):
 
 @app.route('/samples/<sample>')
 def otu_value(sample):
-    from functions import find_col_names
+    # from functions import find_col_names # for local hosting only
+    from bb_app.functions import find_col_names
 
     if sample in find_col_names(engine, 'samples'):
         fetch = engine.execute(f'select "{sample}", otu_id from samples where "{sample}" > 0 order by "{sample}" desc' ).fetchall()
@@ -98,4 +102,5 @@ def otu_value(sample):
 if __name__ == "__main__":
     # app.jinja_env.auto_reload = True
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run()
